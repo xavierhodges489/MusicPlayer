22362a891d986c1f96de982bbfe89674de5ca403
xh00125 <35543992+xh00125@users.noreply.github.com>
2019-03-21 18:08:41 -0400

    Add files via upload

---
+++ MusicPlayer/MusicPlayer/ListView.xaml.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace MusicPlayer
+{
+    /// <summary>
+    /// Interaction logic for ListView.xaml
+    /// </summary>
+    public partial class ListView : Page
+    {
+        public ListView()
+        {
+            InitializeComponent();
+        }
+
+        private void ToAlbumView_Click(object sender, RoutedEventArgs e)
+        {
+            this.NavigationService.Navigate(new AlbumView());
+        }
+    }
+}
---
+++ MusicPlayer/MusicPlayer/MainWindow.xaml.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace MusicPlayer
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void mainWindow_Loaded(object sender, RoutedEventArgs e)
+        {
+            frame.Navigate(new ListView());
+            this.DataContext = this;
+        }
+
+        /*private void Grid_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+            this.DragMove();
+        }*/
+
+        private void Exit_Click(object sender, RoutedEventArgs e)
+        {
+            App.Current.Shutdown();
+        }
+
+        private void DragBar_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+            this.DragMove();
+        }
+
+        private void Skip_back_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+    }
+}

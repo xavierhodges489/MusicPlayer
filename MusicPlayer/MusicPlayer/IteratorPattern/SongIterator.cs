95f460042d1ec65150fd783b1f42ffe1744c26d1
Jenna Lovett <jennablair12@gmail.com>
2019-04-23 14:36:28 -0400

    Update SongIterator.cs

--- MusicPlayer/MusicPlayer/IteratorPattern/SongIterator.cs
+++ MusicPlayer/MusicPlayer/IteratorPattern/SongIterator.cs
@@ -11,12 +11,12 @@ namespace MusicPlayer.IteratorPattern
     class SongIterator : IteratorCreator
     {
         public ObservableCollection<Song> songItems;
-
+        //constructor
         public SongIterator()
         {
             songItems = new ObservableCollection<Song>();
         }
-
+        //adds item to songItems
         public void addItem(Uri file, string title, string album, string artist, int year)
         {
             Song item = new Song(file, title, album, artist, year);
@@ -28,7 +28,7 @@ namespace MusicPlayer.IteratorPattern
             //Song item = song;
             songItems.Add(song);
         }
-
+        //creates iterator for this song iterator
         IEnumerator<Song> IteratorCreator.createIterator()
         {
             return songItems.GetEnumerator();

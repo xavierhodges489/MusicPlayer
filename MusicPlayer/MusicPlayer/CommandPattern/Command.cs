e8cec8227a7683c363611037074cdcfc9fee4ef4
Jenna Lovett <jennablair12@gmail.com>
2019-04-23 14:29:57 -0400

    Update Command.cs

--- MusicPlayer/MusicPlayer/CommandPattern/Command.cs
+++ MusicPlayer/MusicPlayer/CommandPattern/Command.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-
+//Command interface that holds the execute function that all commands will use
 namespace MusicPlayer.CommandPattern
 {
     interface Command

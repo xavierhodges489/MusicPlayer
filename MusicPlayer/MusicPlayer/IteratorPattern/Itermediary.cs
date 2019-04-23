d32b3bb1aebee7bfac0ae0304a1dcac9a903e353
Jenna Lovett <jennablair12@gmail.com>
2019-04-23 14:35:40 -0400

    Update Itermediary.cs

--- MusicPlayer/MusicPlayer/IteratorPattern/Itermediary.cs
+++ MusicPlayer/MusicPlayer/IteratorPattern/Itermediary.cs
@@ -5,25 +5,27 @@ using System.Collections.ObjectModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-
+// communicates between client and iterator creator implementation
 namespace MusicPlayer.IteratorPattern
 {
     class Itermediary
     {
+        //has a song iterator
         IteratorCreator songIterator;
         public ObservableCollection<Song> example;
+        //constructor
         public Itermediary(IteratorCreator songIterator)
         {
             this.songIterator = songIterator;
             example = new ObservableCollection<Song>();
         }
-
+        //prints songs in the iterable list
         public ObservableCollection<Song> printInventory()
         {
             IEnumerator<Song> songIteratorIterator = songIterator.createIterator();
             return printShop(songIteratorIterator);
         }
-
+        //helper method that makes use of the IteratorCreator functions
         private ObservableCollection<Song> printShop(IEnumerator<Song> iterator)
         {
             bool moving = iterator.MoveNext();
